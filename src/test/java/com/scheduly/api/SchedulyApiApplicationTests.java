10912e9c5078db5d162950cccfdd923ac207ba69
Valnei <valneipereirabarboza@gmail.com>
2025-12-09 02:50:32 -0300

    fix(test): configure H2 database for local tests and context loading

--- src/test/java/com/scheduly/api/SchedulyApiApplicationTests.java
+++ src/test/java/com/scheduly/api/SchedulyApiApplicationTests.java
@@ -2,9 +2,21 @@ package com.scheduly.api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
-class SchedulyApiApplicationTests {
+@ActiveProfiles("test")
+@TestPropertySource(properties = {
+		"spring.datasource.url=jdbc:h2:mem:schedulydb_test;DB_CLOSE_DELAY=-1",
+		"spring.datasource.driverClassName=org.h2.Driver",
+		"spring.datasource.username=sa",
+		"spring.datasource.password=",
+		"spring.jpa.database-platform=org.hibernate.dialect.H2Dialect",
+		"spring.jpa.hibernate.ddl-auto=create-drop"
+})
+public class SchedulyApiApplicationTests {
 
 	@Test
 	void contextLoads() {
